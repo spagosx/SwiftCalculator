4cd93e43b4f5b083b3a42273bb9a2b0fe8984832
Spagnolo, Daniele <Daniele.Spagnolo@paddypower.com>
2014-06-06 18:33:22 +0100

    Initial Commit

---
+++ SwiftCalculator/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SwiftCalculator
+//
+//  Created by Spagnolo, Daniele on 06/06/2014.
+//  Copyright (c) 2014 Handstream. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+                            
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
